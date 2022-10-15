1468218d476b4fe5d6cbb38fd41689d02ed4c673
BLACKPARTHA <101777655+BLACKPARTHA@users.noreply.github.com>
2022-10-15 06:53:47 +0530

    Add files via upload

---
+++ NumberPattern.js
@@ -0,0 +1,16 @@
+
+//This is Mine new Program for printing a number pattern 
+//through for loop
+
+
+let n = 5; // height of pattern
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+  // Internal loop
+  for (let j = 1; j <= i; j++) {
+    string += j;
+  }
+  string += "\n";
+}
+console.log(string);
